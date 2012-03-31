330a62f970fba2e2ef296eaa8c5526f28230df03
abak <adrien.bak@gmail.com>
2012-03-31 11:23:14 +0200

    added parsing interface

--- src/file_parser.hpp
+++ src/file_parser.hpp
@@ -1,8 +1,67 @@
 #if !defined(FILE_PARSER)
 #define FILE_PARSER
 
+#include "array.hpp"
+#include <iostream>
+#include <fstrem>
+#include <string>
 
 
+namespace ail
+{
+  
+ class parser
+  {
+    
+  public:
+    parser()
+    {}
+    
+    parser(std::string i_filename):
+      filename(i_filename),
+      file_stream(i_filename)//should be ok, all files are supposed to be in plain text
+    {
+    }
+    
+    ~parser()
+    {
+      file_stream.close();
+    }
+    
+    
+    
+    private :
+    std::string filename;
+    std::ifstream file_stream;
+    
+    
+  public:
+    template <typename PixelType>
+    Array2D<PixelType>& parse_file(std::string i_filename)
+    {
+      filename = i_filename;
+      file_stream.open(i_filename);
+      
+      return parse_file();
+      
+    }
+    
+    template <typename PixelType>
+    Array2D<PixelType>& parse_file()
+    {
+      
+      
+      
+    }
+    
+  };
+  
+  
+  
+  
+  
+  
+}
 
 
 
